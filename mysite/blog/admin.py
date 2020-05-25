b9a7aa0ae1b734672513b065ab293f54c25c26ec
Alexey <nc1ty.alex@gmail.com>
2020-05-25 19:34:43 +0300

    My Django app, first commit

---
+++ mysite/blog/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Post
+
+admin.site.register(Post)
+#Как ты можешь заметить,
+ #Post, которую определили в предыдущей главе.
+  #Чтобы наша модель стала доступна на странице администрирования,
+   #нам нужно зарегистрировать её при помощи admin.site.register(Post).
